75ca44aafdce03826bd413331a6b93c03e0c7550
Grace Tan <gracetan37@gmail.com>
2019-06-11 20:17:59 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,11 +6,11 @@ def key_for_min_value(name_hash)
   lowest_key = nil
  name_hash.each do |name,number|
   if lowest == nil
-    lowest = number
+    lowest = number #starting adding the values to lowest variable as a starting comparison point
     lowest_key = name
   elsif 
     lowest > number 
-    lowest = number
+    lowest = number #this action replaces the current number in lowest variable with the new number that is lower than the current one
     lowest_key = name
 end
 end
